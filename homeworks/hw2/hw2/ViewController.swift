50270a335d2ceee8412767f3d62c7ab86b202bc1
duruaydogdu <duruaydogdu02@gmail.com>
2025-04-17 13:14:59 +0300

    created hw2

---
+++ homeworks/hw2/hw2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2
+//
+//  Created by Duru Aydoğdu on 17.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
