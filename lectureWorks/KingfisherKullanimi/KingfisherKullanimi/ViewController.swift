8ea9eb41dcda2329a2162bcd50655c6511eb867e
duruaydogdu <duruaydogdu02@gmail.com>
2025-04-24 16:34:35 +0300

    add KingfisherKullanimi

---
+++ lectureWorks/KingfisherKullanimi/KingfisherKullanimi/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  KingfisherKullanimi
+//
+//  Created by Duru Aydoğdu on 24.04.2025.
+//
+
+import UIKit
+import Kingfisher
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resimGoster(resimAdi: "ruj.png")
+    }
+
+    
+    @IBAction func buttonResim1(_ sender: Any) {
+        resimGoster(resimAdi: "saat.png")
+    }
+    
+    @IBAction func buttonResim2(_ sender: Any) {
+        resimGoster(resimAdi: "sapka.png")
+    }
+    
+    func resimGoster(resimAdi:String){
+        if let url = URL(string: "http://kasimadalan.pe.hu/urunler/resimler/\(resimAdi)") {
+            DispatchQueue.main.async {
+                self.imageView.kf.setImage(with: url)
+            }
+        }
+    }
+    
+}
